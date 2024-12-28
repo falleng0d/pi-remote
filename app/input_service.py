97efdb2c61123b0f06bcff17f3ed864eaaed5f1c
Mateus Junior <mateus@matj.dev>
2024-12-28 20:32:34 +0100

    feat(app): Implement press_key method in InputService

--- app/input_service.py
+++ app/input_service.py
@@ -1,3 +1,12 @@
+from hid.keyboard import send_keystroke
+from request_parsers.keystroke import Keystroke
+
 
 class InputService:
-    pass
+    keyboard_path: str
+
+    def __init__(self, keyboard_path: str):
+        self.keyboard_path = keyboard_path
+
+    async def press_key(self, key: Keystroke):
+        send_keystroke(self.keyboard_path, key)
